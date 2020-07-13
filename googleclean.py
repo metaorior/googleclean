0e3597b3e1cec382e830a76a4ca24c0dd1c21470
oiacow <oiacow@gmail.com>
2020-07-13 01:30:34 -0300

    testing prototype

---
+++ googleclean.py
@@ -0,0 +1,18 @@
+import json
+
+#Annouce the tool
+print("Welcome to my learning world")
+print("This is a tool for searching in google with custom filters")
+print("2020 July, Happy birthday ")
+
+palavara = input("digite uma palavra teste")
+inputGrabber(palavra)
+
+# def inputGrabber(str1,str2):
+#     filteredData = 
+
+def inputGrabber(str1):
+    filteredData = palavr
+
+
+
